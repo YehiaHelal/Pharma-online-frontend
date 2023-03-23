edbff2c1c1ad0f6b2ea06793de4fee06ccb59798
Yehia Helal <mangetsuhozuki287@yahoo.com>
2023-03-23 13:38:51 +0200

    15

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -85,9 +85,16 @@ export const LoginAction = async ({ request }) => {
     {
       withCredentials: true,
       headers: {
+        "Access-Control-Allow-Credentials": "true",
         "Access-Control-Allow-Origin": "*",
-        "Content-Type": "application/json",
+        "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+        "Access-Control-Allow-Headers":
+          "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
       },
+      // headers: {
+      //   "Access-Control-Allow-Origin": "*",
+      //   "Content-Type": "application/json",
+      // },
     }
   );
 
